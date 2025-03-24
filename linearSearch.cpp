898e660a135eb3ec7273e8631edd8f53127d1529
AnujChikhale <chikhaleanuj38@gmail.com>
2025-03-24 21:43:18 +0530

    Add files via upload

---
+++ linearSearch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int arr[],int n,int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={0,1,2,3,4,5,6,7};
+    int n=8;
+    int key=4;
+    cout<<linearSearch(arr,n,key);
+    return 0;
+}
